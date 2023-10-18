5ae92fdbbf45816c66d11ad40e5355cb4987a7f4
tamimhasan19702 <tamimhasancu@gmail.com>
2023-10-19 02:05:17 +0600

    html added to my app

--- App.js
+++ App.js
@@ -1,22 +1,30 @@
 /** @format */
 
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { View, Text, TextInput, Button } from "react-native";
+import styles from "./styles";
+import { Picker } from "@react-native-picker/picker";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>hello world</Text>
-      <StatusBar style="auto" />
+    <View style={styles.wrapper}>
+      <Text>Announce your speech</Text>
+
+      <View style={styles.row}>
+        <Text>Enter Text</Text>
+        <TextInput style={styles.textInput} />
+      </View>
+
+      <View style={styles.row}>
+        <Text>Select Voice</Text>
+        <View style={styles.outer}>
+          <Picker style={styles.select} />
+        </View>
+      </View>
+
+      <Button title="Convert To Speech" onPress={() => {}} />
+      <Button title="Send to Admin Hub" onPress={() => {}} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
